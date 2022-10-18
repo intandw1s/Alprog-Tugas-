fd8549a7d74ef281fc98ddc2852b1c735878b696
intandw1s <115890877+intandw1s@users.noreply.github.com>
2022-10-19 00:05:19 +0800

    Update Pemograman Fungsi Debit (2205551031_Ni Made Intan Dwi Sukma).c

--- Pemograman Fungsi Debit (2205551031_Ni Made Intan Dwi Sukma).c
+++ Pemograman Fungsi Debit (2205551031_Ni Made Intan Dwi Sukma).c
@@ -1,29 +1,24 @@
 #include<stdio.h>
 
 float hitung_volume_air(float d, float t){
-float volume_air;
-
-volume_air = d * t;
-
-return volume_air;
+    float volume_air;
+    volume_air = d * t;
+    return volume_air;
 }
 
-void main(){
-float debit, waktu, volume_air;
+int main(){
+    float debit, waktu, volume_air;
 
     printf("=================================================\n\n");
     printf("          PROGRAM MENGHITUNG VOLUME AIR          \n\n");
     printf("=================================================\n\n");
-
     printf("Input Debit(liter/jam): ");
     scanf("%f", &debit );
     printf("Input Waktu (jam): ");
     scanf("%f", &waktu );
 
     volume_air = debit*waktu;
-
     printf("Jadi, Volume Air anda \t: %.2f liter \n", volume_air );
-
     return 0;
 }
 
