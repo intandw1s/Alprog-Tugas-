10fcd928c8548332e35682bc099d350b89f68b74
intandw1s <115890877+intandw1s@users.noreply.github.com>
2022-10-19 00:06:50 +0800

    Update Pemograman Fungsi Kecepatan (2205551031_Ni Made Intan Dwi Sukma).c

--- Pemograman Fungsi Kecepatan (2205551031_Ni Made Intan Dwi Sukma).c
+++ Pemograman Fungsi Kecepatan (2205551031_Ni Made Intan Dwi Sukma).c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
 float hitung_jarak(float v, float t){
-float jarak;
-
-jarak =  v * t;
-
-return jarak;
+    float jarak;
+    jarak =  v * t;
+    return jarak;
 }
 
-void main(){
-float kecepatan, waktu, Jarak;
+int main(){
+    float kecepatan, waktu, Jarak;
 
     printf("=================================================\n\n");
     printf("          PROGRAM MENGHITUNG JARAK TEMPUH        \n\n");
     printf("=================================================\n\n");
-
     printf("Input Kecepatan(km/jam): ");
     scanf("%f", &kecepatan );
     printf("Input Waktu (jam): ");
     scanf("%f", &waktu );
 
     Jarak = kecepatan*waktu;
-
     printf("Jadi, jarak tempuh anda \t: %.2f km \n", Jarak );
-
     return 0;
 }
